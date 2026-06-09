94d7398b47b0faa62b923948fbc148c2fd8f509d
Daniel Cavalcante <danielcdesouza@gmail.com>
2026-06-09 15:43:39 -0300

    Add create announcenment to security spring config

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/announcements").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/announcements/*").permitAll()
                 .anyRequest().authenticated()
